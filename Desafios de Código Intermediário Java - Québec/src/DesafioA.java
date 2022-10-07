ebc0c577a4232323d0c9862a9363d01d031dd5a9
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-10-06 20:43:39 -0400

    Desafio A - Finalizado

--- Desafios de Código Intermediário Java - Québec/src/DesafioA.java
+++ Desafios de Código Intermediário Java - Québec/src/DesafioA.java
@@ -1,5 +1,28 @@
-public class DesafioA {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class DesafioA{
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        int num = teclado.nextInt();
+        var step = 0;
+
+        while( num > 0 ){
+            if( num % 2 == 0 ){
+                num /= 2;
+            }else{
+                num--;
+            }
+            step++;
+        }
+
+        if( step > 0 ){
+            System.out.println( step );
+        }else{
+            System.out.println( step );
+        }
+
+        teclado.close();
     }
 }
